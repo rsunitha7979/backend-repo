c2ac8f1185205a6bc3b12aa200897654b98cebf8
rsunitha7979 <rsunitha@imedx.com.au>
2024-12-26 10:35:00 +0530

    file edited

--- backend.tf
+++ backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "s3" {
     bucket = "backend-repo-statefile-bucket"
     key = "terraform.tfstate"
+    dynamodb_table = "dynamo-table-state-lock"
     region = "ap-southeast-2"
     }
 }
+
